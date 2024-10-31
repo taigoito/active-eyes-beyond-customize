a62f3e427c23a6b317ba9a631e2a57db6105b807
Taigo Ito <webmaster@qwel.design>
2024-10-31 16:48:53 +0900

    Minor fix

--- js/_footer.js
+++ js/_footer.js
@@ -11,10 +11,11 @@ export default class Footer {
     
     const content = document.createElement('div');
     content.classList.add('c-footerContent');
-    let text = '福井県公安委員会 探偵業届出証明書 第52140124号';
+    let text = '福井県公安委員会 探偵業届出証明書 第52120093号';
     text += '<br>内閣総理大臣認可（一社）日本調査業協会正会員No.2449号';
-    text += '<br>探偵業務取扱者取得 認定番号No.JISA(1)2711-D0277';
-    text += '<br>〒910-0017 福井県福井市文京7ｰ18ｰ28(フクオカビル2階B号室)';
+    text += '<br>探偵業務取扱者取得 認定番号No.JISA（1）2711-D0277';
+    text += '<br>探偵標識 <a class="c-footerContent__click" href="/data/uploads/detectivesign.pdf" target="_brank">クリック表示</a>'
+    text += '<br>〒910-0017 福井県福井市文京7ｰ18ｰ28（フクオカビル2階B号室）';
     content.innerHTML = text;
     footer.appendChild(content);
   }
